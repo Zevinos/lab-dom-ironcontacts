66c5cb788aecba6fb4bf3c78c1c92c94834c7c6b
evanc <evan.coindreau@gmail.com>
2023-12-13 19:28:48 +0100

    not finished with it3 not done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
@@ -27,34 +26,58 @@ exampleRow.innerHTML = `
     </button>
   </td>
 `;
+const myDeleteButton = exampleRow.querySelector(".btn-delete");
+myDeleteButton.addEventListener("click", deleteContact);
+
+const myLikeButton = exampleRow.querySelector(".btn-like");
+myLikeButton.addEventListener("click", likeContact);
 
 tableBody.appendChild(exampleRow);
-
-
-
-
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
+addthreeContacts();
+
+function addthreeContacts(event) {
+  threeContacts.forEach((element) => {
+    const rowByContact = document.createElement("tr");
+
+    rowByContact.innerHTML = `
+    
+  <td>
+  <img src="${element.pictureUrl}" />
+</td>
+<td> ${element.name} </td>
+<td> ${element.popularity.toFixed(2)} </td>
+<td>
+  <button class="btn-delete">Delete</button>
+</td>
+<td>
+  <button class="btn-like">
+    <img src="./images/icon.png" alt="like" />
+  </button>
+</td>
+  `;
+    const myDeleteButton = rowByContact.querySelector(".btn-delete");
+    myDeleteButton.addEventListener("click", deleteContact);
+    tableBody.append(rowByContact);
+  });
+}
+
+// ITERATION 2 - Delete Buttons
+
+function deleteContact(event) {
+  event.target.closest("tr").remove();
+}
+
+// ITERATION 3 - Like Buttons
+function likeContact(event) {
+  event.target.closest("tr").remove();
+}
+
 // Your code goes here ...
-
-
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
-
-  // ITERATION 3 - Like Buttons
-
-  // Your code goes here ...
-
-  
-  
-
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
